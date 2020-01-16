345f368a012d18e6c64e195263da8ab09496a542
1901429 <1901429@uad.ac.uk>
2020-01-16 10:54:26 +0000

    Week1-complete

--- Week1/CMP105App/Level.cpp
+++ Week1/CMP105App/Level.cpp
@@ -5,9 +5,41 @@ Level::Level(sf::RenderWindow* hwnd)
 	window = hwnd;
 
 	// initialise game objects
-	rect.setSize(sf::Vector2f(50, 5));
-	rect.setPosition(100, 100);
-	rect.setFillColor(sf::Color::Red);
+	rect.setSize(sf::Vector2f(25, 25));
+	rect.setPosition(112.5, 112.5);
+	rect.setFillColor(sf::Color::Green);
+
+	
+	rect2.setSize(sf::Vector2f(50, 50));
+	rect2.setPosition(100, 100);
+	rect2.setFillColor(sf::Color::Red);
+
+	rect3.setSize(sf::Vector2f(12.5, 12.5));
+	rect3.setPosition(118.25, 118.25);
+	rect3.setFillColor(sf::Color::Blue);
+	
+	rect4.setSize(sf::Vector2f(75, 55));
+	rect4.setFillColor(sf::Color::Yellow);
+	rect4.setPosition(100, 100);
+
+	//render text
+	if (!font.loadFromFile("font/arial.ttf"))
+	{
+		std::cout << "Error loading font\n";
+	}
+
+	text.setFont(font);
+	text.setString("Hello World");
+	text.setCharacterSize(18);
+	text.setFillColor(sf::Color::Red);
+	text.setPosition(500, 0);
+	
+	
+	circle.setRadius(15);
+	circle.setPosition(300, 300);
+	circle.setFillColor(sf::Color::Blue);
+	circle.setOutlineColor(sf::Color::Red);
+	circle.setOutlineThickness(2.f);
 }
 
 Level::~Level()
@@ -23,14 +55,22 @@ void Level::handleInput()
 // Update game objects
 void Level::update()
 {
-	
+	sf::Vector2u pos = window->getSize();
+	rect4.setPosition(600-100, 600-100);
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
-
+	
+	//window->draw(rect2);
+	//window->draw(rect);
+	//window->draw(rect3);
+	//window->draw(circle);
+	window->draw(rect4);
+	window->draw(text);
+	
 	endDraw();
 }
 
@@ -44,3 +84,4 @@ void Level::endDraw()
 {
 	window->display();
 }
+
